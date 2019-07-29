8dece57c6dfcbda0830953feab934a258fa73a76
Tyberius Prime <tyberius_prime@coonabibba.de>
2019-07-29 02:18:48 +0200

    added basic debug handlers

---
+++ src/debug_handlers.rs
@@ -0,0 +1,117 @@
+use crate::handlers::ProcessKeys;
+use crate::key_codes::KeyCode;
+use crate::key_stream::{iter_unhandled_mut, Event, EventStatus};
+use crate::USBKeyOut;
+///handlers that probably are only useful while building a keyboard
+///
+///
+///
+use no_std_compat::prelude::v1::*;
+
+fn nibble_to_keycode(nibble: u8) -> KeyCode {
+    match nibble {
+        0 => KeyCode::Kb0,
+        1 => KeyCode::Kb1,
+        2 => KeyCode::Kb2,
+        3 => KeyCode::Kb3,
+        4 => KeyCode::Kb4,
+        5 => KeyCode::Kb5,
+        6 => KeyCode::Kb6,
+        7 => KeyCode::Kb7,
+        8 => KeyCode::Kb8,
+        9 => KeyCode::Kb9,
+        0xA => KeyCode::A,
+        0xB => KeyCode::B,
+        0xC => KeyCode::C,
+        0xD => KeyCode::D,
+        0xE => KeyCode::E,
+        0xF => KeyCode::F,
+        _ => {
+            panic!("nibble larger than 0xF");
+        }
+    }
+}
+fn transform_u32_to_keycodes(x: u32) -> [KeyCode; 8] {
+    return [
+        nibble_to_keycode(((x >> 32 - 4) & 0xff) as u8),
+        nibble_to_keycode(((x >> 32 - 8) & 0xff) as u8),
+        nibble_to_keycode(((x >> 32 - 12) & 0xff) as u8),
+        nibble_to_keycode(((x >> 32 - 16) & 0xff) as u8),
+        nibble_to_keycode(((x >> 32 - 20) & 0xff) as u8),
+        nibble_to_keycode(((x >> 32 - 24) & 0xff) as u8),
+        nibble_to_keycode(((x >> 32 - 28) & 0xff) as u8),
+        nibble_to_keycode(((x >> 32 - 32) & 0xff) as u8),
+    ];
+}
+
+/// this handler helps you build a translation table for MatrixToStream
+/// by outputing the keycode observed as
+/// .into()<Enter>Hex-Keycode\tKeyCode::
+/// so you can simply enter the keycode on a different
+/// keyboard after pressing a key and later sort by
+pub struct TranslationHelper {}
+impl<T: USBKeyOut> ProcessKeys<T> for TranslationHelper {
+    fn process_keys(&mut self, events: &mut Vec<(Event, EventStatus)>, output: &mut T) -> () {
+        for (e, status) in iter_unhandled_mut(events) {
+            *status = EventStatus::Handled;
+            match e {
+                Event::KeyRelease(kc) => {
+                    output.send_string(".into()");
+                    output.send_keys(&[KeyCode::Enter]);
+                    let codes = transform_u32_to_keycodes(kc.keycode);
+                    for c in &codes {
+                        output.send_keys(&[*c]);
+                    }
+                    output.send_string("\tKeyCode::");
+                }
+                _ => {
+                    *status = EventStatus::Handled;
+                }
+            };
+        }
+    }
+}
+
+/// Debug a keystream at any point in the handling
+/// by adding a DebugStream with a callback that knows
+/// how to write something.
+///
+/// Omits Timeout Events, does not print empty keystreams
+pub struct DebugStream<F> {
+    write_callback: F,
+}
+impl<T: USBKeyOut, F: FnMut(String)> ProcessKeys<T> for DebugStream<F> {
+    fn process_keys(&mut self, events: &mut Vec<(Event, EventStatus)>, _output: &mut T) -> () {
+        if !events.is_empty() {
+            (self.write_callback)("[\n".to_string());
+            for (e, status) in events.iter() {
+                match e {
+                    Event::KeyRelease(kc) => {
+                        (self.write_callback)(format!(
+                            "\t(Event::KeyRelease(Key::new({}, {}, {}, {})",
+                            kc.keycode, kc.ms_since_last, kc.running_number, kc.flag,
+                        ));
+                    }
+                    Event::KeyPress(kc) => {
+                        (self.write_callback)(format!(
+                            "\t(Event::KeyPress(Key::new({}, {}, {}, {})",
+                            kc.keycode, kc.ms_since_last, kc.running_number, kc.flag,
+                        ));
+                    }
+                    Event::TimeOut(_) => {}
+                };
+                match status {
+                    EventStatus::Handled => {
+                        (self.write_callback)("EventStatus::Handled),".to_string())
+                    }
+                    EventStatus::Unhandled => {
+                        (self.write_callback)("EventStatus::Unhandled),".to_string())
+                    }
+                    EventStatus::Ignored => {
+                        (self.write_callback)("EventStatus::Ignored),".to_string())
+                    }
+                }
+            }
+        }
+    }
+}
